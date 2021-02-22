4867de4ed0012a674e40fe1b18ba6322206f3dc6
Ivo Anjo <ivo.anjo@datadoghq.com>
2021-02-22 14:04:39 +0000

    Disable debug output

--- temp-rack-test-app/app/datadog.rb
+++ temp-rack-test-app/app/datadog.rb
@@ -1,7 +1,7 @@
 require 'ddtrace'
 
 Datadog.configure do |c|
-  c.diagnostics.debug = true
+  c.diagnostics.debug = false
   c.analytics_enabled = true
   c.runtime_metrics.enabled = true
   c.use :rack, service_name: 'acme-rack'
