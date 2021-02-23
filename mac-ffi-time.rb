6d4a732ca90435ae663bac7f4da781ec1189b57b
Ivo Anjo <ivo.anjo@datadoghq.com>
2021-02-23 12:14:27 +0000

    WIP ffi

--- mac-ffi-time.rb
+++ mac-ffi-time.rb
@@ -14,7 +14,8 @@ module MacStuff
   extend FFI::Library
   ffi_lib FFI::CURRENT_PROCESS
 
-  attach_function :mach_thread_self, [], :ulong
+  attach_function :mach_thread_self, [], :uint32
+  #attach_function :thread_info, [], :int
 end
 
 Pry.start
